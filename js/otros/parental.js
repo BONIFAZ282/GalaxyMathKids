40da18405fe30d3d9b499508d734969d7d419ab1
BONIFAZ282 <bonihd48@gmail.com>
2023-11-03 12:24:12 -0500

    parental

--- js/otros/parental.js
+++ js/otros/parental.js
@@ -1,40 +1,143 @@
-let tiempoRef = Date.now()
-let cronometrar = false
-let acumulado = 0
+let listaNinos = [];
 
-function iniciar() {
-    cronometrar = true
+const objEmpleado = {
+    id: '',
+    nombre: '',
+    apellido: '',
+    edad: '',
+    tiempo: ''
 }
 
-function pausar() {
-    cronometrar = false
-}
+let editando = false;
 
-function reiniciar() {
-    acumulado = 0
-}
+const formulario = document.querySelector('#formulario');
+const nombreInput = document.querySelector('#nombre');
+const apellidoInput = document.querySelector('#apellido');
+const edadInput = document.querySelector('#edad');
+const tiempoInput = document.querySelector('#tiempo');
+const btnAgregarInput = document.querySelector('#btnAgregar');
 
-setInterval(() => {
-    let tiempo = document.getElementById("tiempo")
-    if (cronometrar) {
-        acumulado += Date.now() - tiempoRef
+formulario.addEventListener('submit', validarFormulario); wwwww
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    if (editando) {
+        editarEmpleado();
+        editando = false;
+    } else {
+        objEmpleado.id = Date.now();
+        objEmpleado.nombre = nombreInput.value;
+        objEmpleado.apellido = apellidoInput.value;
+        objEmpleado.edad = edadInput.value;
+        objEmpleado.tiempo = tiempoInput.value;
+
+        agregarEmpleado();
     }
-    tiempoRef = Date.now()
-    tiempo.innerHTML = formatearMS(acumulado)
-}, 1000 / 60);
+}
 
-function formatearMS(tiempo_ms) {
-    let MS = tiempo_ms % 1000
+function agregarEmpleado() {
 
-    let St = Math.floor(((tiempo_ms - MS) / 1000))
+    listaNinos.push({ ...objEmpleado });
 
-    let S = St % 60
-    let M = Math.floor((St / 60) % 60)
-    let H = Math.floor((St / 60 / 60))
-    Number.prototype.ceros = function (n) {
-        return (this + "").padStart(n, 0)
+    mostrarNinos();
+
+    formulario.reset();
+    limpiarObjeto();
+}
+
+function limpiarObjeto() {
+    objEmpleado.id = '';
+    objEmpleado.nombre = '';
+    objEmpleado.apellido = '';
+    objEmpleado.edad = '';
+    objEmpleado.tiempo = '';
+}
+
+function mostrarNinos() {
+    limpiarHTML();
+
+    const divNinos = document.querySelector('.div-ninos');
+
+    listaNinos.forEach(empleado => {
+        const { id, nombre, apellido, edad, tiempo } = empleado;
+
+        const parrafo = document.createElement('p');
+        parrafo.textContent = `${nombre} - ${apellido} - ${edad} - ${tiempo}`;
+        parrafo.dataset.id = id;
+
+        const editarBoton = document.createElement('button');
+        editarBoton.onclick = () => cargarEmpleado(empleado);
+        editarBoton.textContent = 'Editar';
+        editarBoton.classList.add('btn', 'btn-editar');
+        parrafo.append(editarBoton);
+
+        const eliminarBoton = document.createElement('button');
+        eliminarBoton.onclick = () => eliminarEmpleado(id);
+        eliminarBoton.textContent = 'Eliminar';
+        eliminarBoton.classList.add('btn', 'btn-eliminar');
+        parrafo.append(eliminarBoton);
+
+        const hr = document.createElement('hr');
+
+        divNinos.appendChild(parrafo);
+        divNinos.appendChild(hr);
+    });
+}
+
+function cargarEmpleado(empleado) {
+    const { id, nombre, apellido, edad } = empleado;
+
+    nombreInput.value = nombre;
+    apellidoInput.value = apellido;
+    edadInput.value = edad;
+    tiempoInput.value = tiempo;
+
+    objEmpleado.id = id;
+
+    formulario.querySelector('button[type="submit"]').textContent = 'Actualizar';
+
+    editando = true;
+}
+
+function editarEmpleado() {
+
+    objEmpleado.nombre = nombreInput.value;
+    objEmpleado.apellido = apellidoInput.value;
+    objEmpleado.edad = edadInput.value;
+    objEmpleado.tiempo = tiempoInput.value;
+
+    listaninos.map(empleado => {
+
+        if (empleado.id === objEmpleado.id) {
+            empleado.id = objEmpleado.id;
+            empleado.nombre = objEmpleado.nombre;
+            empleado.apellido = objEmpleado.apellido;
+            empleado.edad = objEmpleado.edad;
+            empleado.tiempo = objEmpleado.tiempo;
+        }
+    });
+
+    limpiarHTML();
+    mostrarNinos();
+    formulario.reset();
+
+    formulario.querySelector('button[type="submit"]').textContent = 'Agregar';
+
+    editando = false;
+}
+
+function eliminarEmpleado(id) {
+
+    listaNinos = listaNinos.filter(empleado => empleado.id !== id);
+
+    limpiarHTML();
+    mostrarNinos();
+}
+
+function limpiarHTML() {
+    const divNinos = document.querySelector('.div-ninos');
+    while (divNinos.firstChild) {
+        divNinos.removeChild(divNinos.firstChild);
     }
-
-    return H.ceros(2) + ":" + M.ceros(2) + ":" + S.ceros(2)
-        + "." + MS.ceros(3)
 }
